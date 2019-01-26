6dc8d1b2caf2822aeefc30b66804510d919ba6d0
Parker Holloway <p-holloway@ti.com>
2019-01-26 15:00:53 -0600

    remove gitignore

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotMap;
 import frc.robot.util.DrivingController;
 import frc.robot.util.Odometer;
