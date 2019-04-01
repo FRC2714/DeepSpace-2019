346e265a3d09a85b308d29c5f7d114eeb34902d1
Gagan Bhat <gaganbhat6@gmail.com>
2019-04-01 17:58:44 -0500

    Post rename commit.

--- src/main/java/frc/robot/autontasks/RightCargo.java
+++ src/main/java/frc/robot/autontasks/RightCargo.java
@@ -3,13 +3,13 @@ package frc.robot.autontasks;
 import frc.robot.util.AutonTask;
 import frc.robot.util.ControlsProcessor;
 
-public class RightCargoHabTwoAuton extends AutonTask {
+public class RightCargo extends AutonTask {
 	/**
 	 * Accepts the object of the running controlsProcessor to modify
 	 *
 	 * @param controlsProcessor
 	 */
-	public RightCargoHabTwoAuton(ControlsProcessor controlsProcessor) {
+	public RightCargo(ControlsProcessor controlsProcessor) {
 		super(controlsProcessor);
 
 		queueTask("valve_off -p");
